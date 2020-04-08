82e1e490a3eaf5f05968a29d40775f5e8df91798
Lselao <lselao@student.wethinkcode.co.za>
2020-04-08 15:33:32 +0200

    Creating MVVM model
    Building a folder structure

---
+++ Stack Overflow API/Stack OverfFlow API/Stack OverfFlow API/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stack OverfFlow API
+//
+//  Created by lebose on 2020/04/08.
+//  Copyright © 2020 lebose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    }
+
+
+}
+
